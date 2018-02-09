23824993ce41a0b2776a86fb5decef43e3728356
tyler carson <tyler.c.carson@gmail.com>
2018-02-08 17:43:25 -0800

    cleanup comments

--- Chapter2/removeDups/removeDups.js
+++ Chapter2/removeDups/removeDups.js
@@ -1,18 +1,16 @@
 function removeDups(linkedList) {
-  //keep a hash table of values
+
   var values = {};
   var node = linkedList.head;
   values[node.val] = true;
-  //iterate through nodes w/ while loop
+
   while (node.next !== null) {
-    //if already exists
+
     if (values[node.next.val]) {
-      //remove
       node.next = node.next.next;
-    } else { //doesn't exist
-      //add to hash table
+      
+    } else {
       values[node.next.val] = true;
-      //iterate
       node = node.next;
     }
   }
--- Chapter2/removeDups/test/removeDups.spec.js
+++ Chapter2/removeDups/test/removeDups.spec.js
@@ -5,15 +5,14 @@ var removeDups = require('../removeDups.js');
 
 describe('Remove Duplicates from Linked List', function() {
   it('should find and remove duplicates', function() {
-    //create a linked list
+
     var LL = new LinkedList(5);
     LL.addToTail(6);
     LL.addToTail(7);
     LL.addToTail(5);
     LL.addToTail(7);
-    //run removeDups
-    LL = removeDups(LL);
-    //run tests on the linked list, should not contain second instances of 5 and 7
+    removeDups(LL);
+
     assert.isNull(LL.head.next.next.next);
   });
 });
